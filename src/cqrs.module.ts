d6a469ec8d7f3254d5b2b1bce417e9364f03f1c9
dunglongtran <dunglongtran@gmail.com>
2019-02-25 23:33:24 +0700

    fix missing  QueryBus

--- src/cqrs.module.ts
+++ src/cqrs.module.ts
@@ -1,10 +1,11 @@
-import { Module } from '@nestjs/common';
+import { Module, Query } from '@nestjs/common';
+import { QueryBus } from './query-bus';
 import { CommandBus } from './command-bus';
 import { EventPublisher } from './event-publisher';
 import { EventBus } from './event-bus';
 
 @Module({
-  providers: [CommandBus, EventBus, EventPublisher],
-  exports: [CommandBus, EventBus, EventPublisher],
+  providers: [QueryBus, CommandBus, EventBus, EventPublisher],
+  exports: [QueryBus, CommandBus, EventBus, EventPublisher],
 })
 export class CQRSModule {}
